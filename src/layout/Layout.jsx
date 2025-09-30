99d0bbb1073024905484eadd47c396bdfadbb613
isabelleadrury <isabelledrury38@gmail.com>
2025-09-30 19:14:04 -0400

    fixed Layout.jsx

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,22 @@
-import Navbar from "./Navbar";
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
 
+const navClass = ({ isActive }) => "link" + (isActive ? "active" : "");
 /** The shared layout for all pages of the app */
 export default function Layout({ children }) {
   return (
     <>
-      <Navbar />
-      <main>{children}</main>
+      <header className="nav">
+        <NavLink to="/" className={navClass}>
+          Home
+        </NavLink>
+        <NavLink to="activities" className={navClass}>
+          Activities
+        </NavLink>
+      </header>
+      <main className="main">
+        <Outlet />
+      </main>
     </>
   );
 }
